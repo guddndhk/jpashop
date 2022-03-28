e547537739ae0bd0d42b3879919dc11c8ebdc2bf
guddndhk <guddndhk@naver.com>
2022-03-29 03:49:03 +0900

    did u trust ur self? i'm not. i only trust my persistence. like coding this eating my persistence and give m back to understanding  also never gonna betrayal to me.
     22/03/29
     MemberServiceTest 개발
     개발 진행 내용
     회원가입 완료.
     중복_회원_예외 완료.
     Try Catch 문을 사용하지 않고(사실 사용하지만 캡슐화)
     @Test(expected = IllegalStateException.class)
     을 사용하여 코드를 간결하게 작성 하였다. 나도 오늘 처음 알게된사실
     코드가 너무 깔끔해지고 가독성도 나쁘지 않다. 개발을 하면 할수록 개발자들은 대단하지만
     진짜 귀찮은걸 싫어 하는 사람들이다 ㅋㅋㅋㅋㅋㅋ

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +24,7 @@ public class MemberServiceTest {
     @Autowired MemberRepository memberRepository;
     @Autowired EntityManager em;
 
+    // 테스트 코드 작성시 테스트 메서드명을 한글로 만드는편이다. 테스트안에서는 크게 문제되지 않고 보기 편하다..
     @Test
     public void 회원가입() throws Exception {
         //given
@@ -37,13 +39,21 @@ public class MemberServiceTest {
         assertEquals(member, memberRepository.findOne(saveId));
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
         //given
+        Member member1 = new Member();
+        member1.setName("jo");
+
+        Member member2 = new Member();
+        member2.setName("jo");
 
         //when
+        memberService.join(member1);
+        memberService.join(member2);// 같은 이름으로 2개가 들어갔으니 예외가 나와야한다.
 
         //then
+        fail("예외가 발생 하여야함");
     }
 
 }

a961fbd2fd590f1f3c17de659f5f469489b9ea4d
guddndhk <guddndhk@naver.com>
2022-03-30 16:34:14 +0900

    did u trust ur self? i'm not. i only trust my persistence. like coding this eating my persistence and give m back to understanding  also never gonna betrayal to me.
     22/03/29
     상품 엔티티 개발 Item.java
     개발 진행 내용
     상품 + 기능
     상품 - 기능
    
     == 부가설명==
     setter 을 남발하여 값을 조정할 생각보다는 stockQuantity 을 변경할 일이 있으면
     핵심 비즈니스 메서드 로직을 만들어서 변경 하는것이 좀더 객체지향적인 개발 방법이다.

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,21 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==
+    /*
+    stock 증가 로직
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
+//setter 을 남발 하지말고 stockQuantity 변경할 일이 있으면 핵심 비즈니스 메서드를 만들어서 변경하는것이 바람직하다.

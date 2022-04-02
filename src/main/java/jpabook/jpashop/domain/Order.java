c34646ed017a2e82b94ba5bade20127762953be0
guddndhk <guddndhk@naver.com>
2022-04-03 00:25:52 +0900

    did u trust ur self? i'm not. i only trust my persistence. like coding this eating my persistence and give m back to understanding  also never gonna betrayal to me.
     22/04/02
     주문, 주문상품 엔티티 개발
     개발 진행 상황
     오더 주문 생성 메서드 작성.
     오더 주문 취소 메서드 작성.
     오더 주문 조회 가격 작성.
    
     오더아이템 생성 메서드 작성.
     오더아이템 취소 메서드 작성.
     오더아이템 주문 상품 전체 가격 조회 메서드.
    
     == 부가설명==

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -82,4 +82,16 @@ public class Order {
             orderItem.cancel();
         }
     }
+
+    //==조회 로직==
+    //전체 주문 조회 가격
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+
+//       return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+    }
 }

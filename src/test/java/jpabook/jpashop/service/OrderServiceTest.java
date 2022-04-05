f58dad78ba5db63521bbf13c348e31d8067ee1b7
guddndhk <guddndhk@naver.com>
2022-04-06 00:10:41 +0900

    did u trust ur self? i'm not. i only trust my persistence. like coding this eating my persistence and give m back to understanding  also never gonna betrayal to me.
     22/04/04
     개발 진행 상황
     오더 서비스 테스트 진행.
    
     개발 내용
     상품주문 테스트 완료.
     상품주문_재고수량초과 테스트 완료.
    
     == 부가설명==

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -6,8 +6,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import jpabook.jpashop.repository.OrderRepository;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,16 +36,9 @@ public class OrderServiceTest {
     @Test
     public void 상품주문() throws Exception {
         //given
-        Member member = new Member();
-        member.setName("회원1");
-        member.setAddress(new Address("서울", "강가", "123-123"));
-        em.persist(member);
+        Member member = createMember();
 
-        Book book = new Book();
-        book.setName("사골 JPA");
-        book.setPrice(10000);
-        book.setStockQuantity(10);
-        em.persist(book);
+        Book book = createBook("사골 JPA", 10000, 10);
 
         int orderCount = 2;
 
@@ -62,6 +55,21 @@ public class OrderServiceTest {
 
     }
 
+    @Test(expected = NotEnoughStockException.class)
+    public void 상품주문_재고수량초과() throws Exception{
+        //given
+        Member member = createMember();
+        Item item = createBook("사골 JPA", 10000, 10);
+
+        int orderCount = 11;
+
+        //when
+        orderService.order(member.getId(), item.getId(), orderCount);
+
+        //then
+        fail("재고 수량 부족 예외가 발생하여야 한다.");
+    }
+
     @Test
     public void 주문취소() throws Exception {
         //given
@@ -71,12 +79,20 @@ public class OrderServiceTest {
         //then
     }
 
-    @Test
-    public void 상품주문_재고수량초과() throws Exception {
-        //given
+    private Book createBook(String name, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        em.persist(book);
+        return book;
+    }
 
-        //when
-
-        //then
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "강가", "123-123"));
+        em.persist(member);
+        return member;
     }
 }
